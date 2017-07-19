ca73bec7ca02b18d2d00b57642827d74223de262
microjafar <tljafer@gmail.com>
2017-07-19 23:22:09 +0530

    Initial

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+hello
+</body>
+</html>
+<?php
+//$token ="364969879:AAFJHH8N20fMwDgkji33qrfc6VngnY1N25E";
+//$website="api.telegram.org/bot".$token;
+//$update = file_get_contents('php://input');
+//$update = json_decode($update,true);
+//
+//$chatId = $update["message"]["chat"]["id"];
+//$message = $update["message"]["text"];
+//
+//switch ($message){
+//	case "/start":
+//		sendMessage($chatId,"Thank you for joining");
+//		break;
+//	default:
+//		sendMessage($chatId,"DEFAULT");
+//}
+//
+//function sendMessage($chatId, $message){
+//	$url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+//	file_get_contents($url);
+//}
+
+
